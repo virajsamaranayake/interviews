1e65ad7910a8efb0c3792c66ed6ac2e1d81421b3
virajsamaranayake <32884092+virajsamaranayake@users.noreply.github.com>
2017-10-17 17:52:45 -0400

    Update Exercise1.java
    Full Name       : Viraj Samaranayake
    Email Address   : virajsamaranayake@yahoo.co.uk
    Recruiter Name  : Jason Biser from Apex Systems

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -6,37 +6,42 @@ import java.util.Set;
 
 TODO: Make this class immutable.
 
+Make immutable class need to do below steps
+1. Declare the calss as final 
+2. Make all fields private
+3. Don't provide Setters 
+4. Make all mutable fields final 
+5. Initialize all the fields via constructor 
+6. Perform cloning of Objects in the getter methods 
+
 */
-public class Exercise1 {
+public final class Exercise1 {
 
 	public static class Person {
 		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
+		private final Set<String> phoneNumbers;
+		private final String firstName;
+		private final String lastName;
 		
-		public Person() {
+		public Person(Set<String> phoneNumbers,String firstName,String lastName ) {
+			this.phoneNumbers = phoneNumbers;
+			this.firstName = firstname;
+			this.lastName = lastname;
 		}
 
 		public Set<String> getPhoneNumbers() {
-			return phoneNumbers;
+			return (Set<String>)phoneNumbers.clone();
 		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-			phoneNumbers = newPhoneNumbers;
-		}
+		
 		
 		public String getFirstName() {
-			return firstName;
-		}
-		public void setFirstName(String newName) {
-			firstName = newName;
+			return firstName.clone();
 		}
 		
+		
 		public String getLastName() {
-			return lastName;
+			return lastName.clone();
 		}
-		public void setLastName(String newName) {
-			lastName = newName;
-		}
+		
 	}
 }
